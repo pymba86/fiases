11faa90d6ca82fa6be99b8374fc73bd3865187c4
Artem Matveychuk <pymba1996@live.com>
2019-02-08 16:53:44 +0500

    Допушена ошибка в тестах

--- src/tests/Units/BaseTest.php
+++ src/tests/Units/BaseTest.php
@@ -11,6 +11,6 @@ class BaseTest extends UnitTestCase
 {
     public function testBaseCase()
     {
-        $this->assertTrue(extension_loaded('phalcon'));
+        $this->assertTrue(extension_loaded('phalcon1'));
     }
 }
