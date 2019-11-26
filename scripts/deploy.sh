e2eb356242172e3bcb577d016d246ad03e295fcd
Artem Matveychuk <pymba1996@live.com>
2019-11-26 16:13:00 +0500

    Конфигурация для деплоя на сервер

---
+++ scripts/deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# Exit in case of error
+set -e
+
+DOMAIN=${DOMAIN} \
+PATH_PREFIX=${PATH_PREFIX} \
+TRAEFIK_TAG=${TRAEFIK_TAG} \
+STACK_NAME=${STACK_NAME} \
+TAG=${TAG} \
+docker-compose \
+-f docker-compose.shared.base-images.yml \
+-f docker-compose.shared.depends.yml \
+-f docker-compose.deploy.env.yml \
+-f docker-compose.deploy.images.yml \
+-f docker-compose.deploy.command.yml \
+-f docker-compose.deploy.labels.yml \
+-f docker-compose.deploy.config.yml \
+-f docker-compose.deploy.ports.yml \
+-f docker-compose.deploy.networks.yml \
+-f docker-compose.deploy.volumes-placement.yml \
+config > docker-stack.yml
+
+docker-auto-labels docker-stack.yml
+
+docker stack deploy -c docker-stack.yml --with-registry-auth ${STACK_NAME}
